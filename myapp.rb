e9a166311c67408791762b281bcc9bd2c0e7280b
pollseed <cb.so.devope+0001@gmail.com>
2015-12-18 01:46:22 +0900

    add setting1

--- myapp.rb
+++ myapp.rb
@@ -1,17 +1,48 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
+
+set :public_folder, File.dirname(__FILE__) + '/public'
+
+helpers do
+  def now
+    Time.now
+  end
+end
+
+class Stream
+  def each
+    100.times { |i| yield "#{i}\n" }
+  end
+
+  def default(title, content)
+    @title = title
+    @content = content
+  end
+end
+
+before do
+  cache_control :public, :max_age => 0
+end
 
 get '/' do
-    'Hello, World!'
+  Stream.new.default('hoge', "hogehogeTODO")
 end
 
-post '/' do
-    'Howdy'
+
+get '/:title' do
+  Stream.new.default(params['title'], "hogehogeTODO")
 end
+
+post ('/') { Stream.new }
 
 get '/hello/:name' do
-    "Hello, #{params['name']}!"
+  "Hello, #{params['name']}!"
 end
 
 get '/hello2/:name' do |n|
-    "Howdy, #{n}!"
+  "Howdy, #{n}!"
+end
+
+get '/now' do
+  erb "<%= now() %>"
 end
