f29033ba256cec94407e73846e7a8f2d3e974db3
pollseed <cb.so.devope+0001@gmail.com>
2015-12-17 22:54:22 +0900

    add routing test

--- myapp.rb
+++ myapp.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
     'Hello, World!'
 end
+
+post '/' do
+    'Howdy'
+end
+
+get '/hello/:name' do
+    "Hello, #{params['name']}!"
+end
+
+get '/hello2/:name' do |n|
+    "Howdy, #{n}!"
+end
